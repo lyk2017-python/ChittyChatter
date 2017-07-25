fdbd2d1eb122d8e61ea16193bef8ffbfe5dbea26
g3vxy <g3vxyv@gmail.com>
2017-07-25 19:27:20 +0300

    Modeller tamamlandı.

--- ccforum/forum/models.py
+++ ccforum/forum/models.py
@@ -13,14 +13,20 @@ from django.db import models
      (|     | )
     /'\_   _/`\
     \___)=(___/
-
+    Medusa Rocks ^^
 """
 
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+
+class Thread(models.Model):
+    title = models.CharField(max_length=50)
+    category = models.ForeignKey(Category)
+
 class Post(models.Model):
-    post_title = models.CharField(max_length=50) # Medusa rocks. ^^
-    post_text = models.TextField() # This is the place where we are taking post.
-    post_stime = models.DateTimeField(auto_now_add=True) # This is for sent time.
-    post_ctime = models.DateTimeField()  # This is for change time.
-    post_thread = models.ManyToOneRel() # This is for relation of posts and threads.
-    post_like = models.PositiveSmallIntegerField() # This is the place where we are keeping like count.
-    post_is_reported = models.BooleanField(default=False) # This field for reporting stuff.
+    content_text = models.TextField() # This is the place where we are taking post.
+    sent_date = models.DateTimeField(auto_now_add=True) # This is for sent time.
+    change_date = models.DateTimeField()  # This is for change time.
+    thread = models.ForeignKey(Thread) # This is for relation of posts and threads.--uu-
+    like = models.PositiveSmallIntegerField() # This is the place where we are keeping like count.
+    is_reported = models.BooleanField(default=False) # This field for reporting stuff
