78b474bbc8a2b7822eaf94a29eb174448bcd1960
g3vxy <g3vxyv@gmail.com>
2017-07-25 21:14:12 +0300

    Query edilen nesneler artık title döndüyorlar.

--- ccforum/forum/models.py
+++ ccforum/forum/models.py
@@ -17,16 +17,25 @@ from django.db import models
 """
 
 class Category(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50,unique=True)
+
+    def __str__(self):
+        return "{}".format(self.title)
 
 class Thread(models.Model):
     title = models.CharField(max_length=50)
     category = models.ForeignKey(Category)
 
+    def __str__(self):
+        return "{}".format(self.title)
+
 class Post(models.Model):
     content_text = models.TextField() # This is the place where we are taking post.
     sent_date = models.DateTimeField(auto_now_add=True) # This is for sent time.
-    change_date = models.DateTimeField()  # This is for change time.
+    change_date = models.DateTimeField(auto_now=True)  # This is for change time.
     thread = models.ForeignKey(Thread) # This is for relation of posts and threads.--uu-
-    like = models.PositiveSmallIntegerField() # This is the place where we are keeping like count.
+    like = models.PositiveSmallIntegerField(default=0) # This is the place where we are keeping like count.
     is_reported = models.BooleanField(default=False) # This field for reporting stuff
+
+    def __str__(self):
+        return "{} thread, {}. post".format(Thread.title, self.id)
