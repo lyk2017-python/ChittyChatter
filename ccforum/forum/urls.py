c2458b2a62667d239e0997232b2e4d4e659ac038
g3vxy <g3vxyv@gmail.com>
2017-07-27 10:47:58 +0300

    Template'ler düzenlendi.

--- ccforum/forum/urls.py
+++ ccforum/forum/urls.py
@@ -4,5 +4,5 @@ from forum.views import *
 urlpatterns = [
     url(r'^$', CategoryView.as_view(), name="home"),
     url(r'^(?P<slug>[A-Za-z0-9\-]+)$', CategoryDetailView.as_view(), name= "category_name"),
-    url(r'^thread/(?P<slug>[A-Za-z0-9\-]+)-(?P<pk>[0-9]+)$', ThreadView.as_view()),
+    url(r'^thread/(?P<slug>[A-Za-z0-9\-]+)-(?P<pk>[0-9]+)$', ThreadView.as_view(), name="thread_name"),
 ]
--- ccforum/forum/views.py
+++ ccforum/forum/views.py
@@ -8,5 +8,14 @@ class CategoryView(ListView):
 class CategoryDetailView(DetailView):
     model = Category
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["thread"] = Thread.objects.all()
+        return context
 class ThreadView(DetailView):
     model = Thread
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["category"] = Category.objects.all()
+        return context
