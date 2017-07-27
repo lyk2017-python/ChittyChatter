95a928644ca32b14623a17ad4e15bbe1639d3022
g3vxy <g3vxyv@gmail.com>
2017-07-27 11:42:06 +0300

    Templateler düzenlendi.

--- ccforum/forum/views.py
+++ ccforum/forum/views.py
@@ -18,4 +18,5 @@ class ThreadView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["category"] = Category.objects.all()
+        context["post"] = Post.objects.all()
         return context
