461d89fdae44dcc3c22cae3a448f2df5dfcc06a2
Berk Buzcu <buzcuberk@gmail.com>
2017-07-28 10:01:36 +0300

    Issue #2

--- ccforum/forum/views.py
+++ ccforum/forum/views.py
@@ -20,5 +20,5 @@ class ThreadView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["category"] = Category.objects.all()
-        context["post"] = Post.objects.all()
+        context["post"] = context["object"].post_set.all()
         return context
